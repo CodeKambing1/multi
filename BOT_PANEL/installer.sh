be2a537a3ebc3720602a0564338c52cb32d29df6
CodeKambing1 <112297720+CodeKambing1@users.noreply.github.com>
2022-09-23 17:40:20 +0800

    Add files via upload

--- BOT_PANEL/installer.sh
+++ BOT_PANEL/installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 ipes=$(curl -sS ipv4.icanhazip.com)
-surat=$(curl -sS https://raw.githubusercontent.com/techsolution11/access/main/bot | grep -w $ipes | awk '{print $4}')
+surat=$(curl -sS https://raw.githubusercontent.com/CodeKambing1/access/main/ip | grep -w $ipes | awk '{print $4}')
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 if [[ "$surat" = "true" ]]; then
   echo -ne
